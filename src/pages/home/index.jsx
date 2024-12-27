64132ac6d661ee2defd925115eabf2d399d0ddc1
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-12-27 10:28:03 +0100

    feat: testimonials

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './partials/hero'
 import WeekSpecials from './partials/weekSpecial'
+import Testimonials from './partials/testimonials'
 
 export default function Home() {
   return (
     <>
         <Hero/>
         <WeekSpecials/>
+        <Testimonials/>
     </>
   )
 }
---
+++ src/pages/home/partials/testimonials/partials/card/index.jsx
@@ -0,0 +1,27 @@
+import {
+  faStar,
+  faStarHalfStroke
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './styles.css';
+
+const ratingLevels = { '0.5': faStarHalfStroke, '1': faStar, };
+
+export function Card({ customer }) {
+  return (
+    <article className="testimonial-card">
+      <img src={customer.image} alt={customer.fullName} />
+      <h4>{customer.fullName}</h4>
+      <span>
+        {customer.rating.map((ratingPoint, index) => 
+          <FontAwesomeIcon 
+            key={index} 
+            icon={ratingLevels[ratingPoint]} 
+            size="xs" 
+          />
+        )}
+      </span>
+      <blockquote><p>"{customer.says}"</p></blockquote>
+    </article>
+  );
+};
