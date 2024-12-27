df647525ee07c8c671bee93213c8cb7ec9cec3eb
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-12-27 09:58:31 +0100

    feat: not found page

--- src/pages/notfound/index.jsx
+++ src/pages/notfound/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import './styles.css'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div id='not-found' className="container">
+        <h2>Page not found</h2>
+    </div>
   )
 }
