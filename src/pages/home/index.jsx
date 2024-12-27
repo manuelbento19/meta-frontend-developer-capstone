5240ce8403b7690a7e0e5fcdc64d4ba6deaa8830
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-12-27 10:23:08 +0100

    feat: week specials section

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './partials/hero'
+import WeekSpecials from './partials/weekSpecial'
 
 export default function Home() {
   return (
     <>
         <Hero/>
+        <WeekSpecials/>
     </>
   )
 }
---
+++ src/pages/home/partials/weekSpecial/partials/card/index.jsx
@@ -0,0 +1,25 @@
+import { faMotorcycle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import { pages } from '../../../../../../constants';
+import './styles.css';
+
+export function Card ({ meal }) {
+  return (
+    <article className="meal-card">
+      <div className="meal-card-image">
+        <img src={meal.image} alt={meal.name} />
+      </div>
+      <div className="meal-card-header">
+        <h3>{meal.name}</h3>
+        <span>{meal.price}</span>
+      </div>
+      <div className="meal-card-body-footer">
+        <p>{meal.description}</p>
+        <Link to={pages.orderOnline.path}>
+          Order a delivery <FontAwesomeIcon icon={faMotorcycle} />
+        </Link>
+      </div>
+    </article>
+  );
+};
